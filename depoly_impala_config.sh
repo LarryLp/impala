6edab0cf32f10f55d48a322a82a3832c6c85bbeb
LarryLp <jiang-gq@msn.com>
2016-12-12 17:06:10 +0800

    Update depoly_impala_config.sh

--- depoly_impala_config.sh
+++ depoly_impala_config.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 2 ]; then
+  # add metadata info,you should modify it according to your own situation 
+  echo "192.168.0.91 node01" >> /etc/hosts
+  
+  # set service ip 
+  echo 'IMPALA_CATALOG_SERVICE_HOST='$1''> /etc/default/impala
+  echo 'IMPALA_STATE_STORE_HOST='$2'' >> /etc/default/impala
 
-# add metadata info,you should modify it according to your own situation 
-RUN echo "192.168.0.91 node01" >> /etc/hosts
+  # add impala config
+  cat /etc/default/config.txt|col -b >> /etc/default/impala
+  echo "impala config job done!"
+else 
+  echo "Invalid command number"
+  exit 1
+fi
 
-# set container ip address
-echo 'IMPALA_CATALOG_SERVICE_HOST='$(hostname -I|awk '{print $1}') > /etc/default/impala
-echo 'IMPALA_STATE_STORE_HOST='$(hostname -I|awk '{print $1}') >> /etc/default/impala
-
-# add impala config
-cat /etc/default/config.txt|col -b >> /etc/default/impala
-
-echo "impala config job done!"
